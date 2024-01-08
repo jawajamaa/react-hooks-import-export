8ed56defdd525eca85f5bccecf9e82a3236ad0ea
Timothy Ryon <tim@timryon.com>
2024-01-08 13:21:05 -0500

    finish code-along

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+console.log(RMFunctions.trees);
+
+RMFunctions.wildlife();
+
+RMFunctions.elevation();
+
+export default ColoradoStateParks
